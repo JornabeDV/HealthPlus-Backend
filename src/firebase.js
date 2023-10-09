2c6481c15e71b9bb2e66fc6be3a09774cbd0f939
JornabeDV <103864663+JornabeDV@users.noreply.github.com>
2023-10-09 09:53:28 -0300

    cambios firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
+const admin = require('firebase-admin');
 
-const {initializeApp, applicationDefault} = require('firebase-admin/app');
-const {getFirestore} = require('firebase-admin/firestore')
+// Especifica la ruta relativa al archivo JSON de credenciales
+const serviceAccount = require('../firebase.json');
 
-initializeApp({
-    credential: applicationDefault()
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
 });
 
-const db = getFirestore();
+const db = admin.firestore();
 
-module.exports = {db};
+module.exports = { db };
