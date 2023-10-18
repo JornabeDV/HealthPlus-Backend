b85efca02119b10f0185b513118eb7a052e01fb5
JornabeDV <103864663+JornabeDV@users.noreply.github.com>
2023-10-18 10:14:27 -0300

    deploy 10:14 18/10

--- src/handlers/Doctors/getDoctors.js
+++ src/handlers/Doctors/getDoctors.js
@@ -1,29 +1,27 @@
-const { bringDoctors,bringDoctorByName } = require('../../controllers/doctorsControllers');
-
-const getDoctors = async (req, res) => {
-
+const {
+    bringDoctors,
+    bringDoctorByName,
+  } = require("../../controllers/doctorsControllers");
+  
+  const getDoctors = async (req, res) => {
     const { name } = req.query;
-
     if (name) {
-        try {
-            const doctor = await bringDoctorByName(name);
-            res.status(200).json(doctor)
-        } catch (error) {
-            res.status(404).json(error.message)
-        }
-
-    } else {    
-        try {
-            const doctors = await bringDoctors()
-            res.status(200).json(doctors);
-    
-
-        } catch (error) {
-            console.log(error);
-            res.status(404).json(error);
-        }
+      try {
+        const doctor = await bringDoctorByName(name);
+        res.status(200).json(doctor);
+      } catch (error) {
+        res.status(404).json(error.message);
+      }
+    } else {
+      try {
+        const doctors = await bringDoctors();
+        res.status(200).json(doctors);
+      } catch (error) {
+        console.log(error);
+        res.status(404).json(error);
+      }
     }
-
-};
-
-module.exports = getDoctors;
+  };
+  
+  module.exports = getDoctors;
+  
--- src/handlers/Users/bringUsers.js
+++ src/handlers/Users/bringUsers.js
@@ -1,24 +1,26 @@
-const { bringUsers, bringUserByName } = require("../../controllers/usersControllers");
-
-const bringUsersHandler = async (req, res) => {
-
-    const { name } = req.body;
-
+const {
+    bringUsers,
+    bringUsersByName,
+  } = require("../../controllers/usersControllers");
+  
+  const bringUsersHandler = async (req, res) => {
+    const { name } = req.query;
+  
     if (name) {
-        try {
-            const user = await bringUserByName(name);
-            res.status(200).json(user);
-        } catch (error) {
-            res.status(404).json(error.message)
-        }
+      try {
+        const user = await bringUsersByName(name);
+        res.status(200).json(user);
+      } catch (error) {
+        res.status(404).json(error.message);
+      }
     } else {
-        try {
-            const users = await bringUsers();
-            res.status(200).json(users)
-        } catch (error) {
-            res.status(400).json(error.message)
-        }
+      try {
+        const users = await bringUsers();
+        res.status(200).json(users);
+      } catch (error) {
+        res.status(404).json(error.message);
+      }
     }
-};
-
-module.exports = bringUsersHandler;
+  };
+  
+  module.exports = bringUsersHandler;
